ab2def33a4c13935f4c698f1aa119e14728bf5e1
Javier Balsas <javier9518@gmail.com>
2020-04-12 13:00:04 -0500

    container redondeado

--- custom_pinter/lib/src/widgets/header.dart
+++ custom_pinter/lib/src/widgets/header.dart
@@ -12,3 +12,20 @@ class HeaderCuadrado extends StatelessWidget {
     );
   }
 }
+
+class HeaderRededondeado extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+     
+      decoration: BoxDecoration(
+         color: Color(0xff615AAB),
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50),
+        bottomRight: Radius.circular(50)
+          )
+      ),
+    );
+  }
+}
