af8d5e0498a7b7dc893b8e54b562a238c1e82a54
Javier Balsas <javier9518@gmail.com>
2020-04-12 12:53:11 -0500

    container cuadrado

---
+++ custom_pinter/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:custom_pinter/src/pages/headers_pages.dart';
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pointer',
+      home: HeaderPage()
+    );
+  }
+}
---
+++ custom_pinter/lib/src/pages/headers_pages.dart
@@ -0,0 +1,13 @@
+import 'package:custom_pinter/src/widgets/header.dart';
+import 'package:flutter/material.dart';
+
+class HeaderPage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: HeaderCuadrado(),
+    );
+  }
+}
