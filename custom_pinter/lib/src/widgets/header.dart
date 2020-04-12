e6d8a17581b0803e4f09919d464e066d45633dab
Javier Balsas <javier9518@gmail.com>
2020-04-12 14:24:18 -0500

    painter diagonal

--- custom_pinter/lib/src/widgets/header.dart
+++ custom_pinter/lib/src/widgets/header.dart
@@ -29,3 +29,43 @@ class HeaderRededondeado extends StatelessWidget {
     );
   }
 }
+
+class HeaderDiagonal extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+        // color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter{
+
+  @override
+  void paint(Canvas canvas, Size size) {
+   final paint = Paint();
+   paint.color=Color(0xff615AAB);
+   paint.style=PaintingStyle.fill;
+   paint.strokeWidth=20;
+   final path = new Path();
+   path.moveTo(0, size.height*0.35);
+   path.lineTo(size.width,size.height*0.30);
+   path.lineTo(size.width,0);
+   path.lineTo(0, 0);
+    //  path.lineTo(0,size.height*0.5);
+   canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return null;
+  }
+
+}
