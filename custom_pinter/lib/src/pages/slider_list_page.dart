f67856749f19e6c31e7d401127c4d4192716ef52
Javier Balsas <javier9518@gmail.com>
2020-04-17 18:03:38 -0500

    custom slider

---
+++ custom_pinter/lib/src/pages/slider_list_page.dart
@@ -0,0 +1,216 @@
+import 'package:flutter/material.dart';
+
+class SliderListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // body:_ListadeTareas()
+      // body: _Titulo(),
+      body:Stack(children: <Widget>[
+         _MainScroll(),
+         Positioned(
+           bottom: -10,
+           right: 0,
+           child: _BotonNewList())
+      ],),
+    );
+  }
+}
+
+class _BotonNewList extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+   final size = MediaQuery.of(context).size;
+    return ButtonTheme(
+      minWidth: size.width*0.9,
+      height: 100,
+      child: RaisedButton(
+        child: Text(
+          'Create new List',
+          style: TextStyle(
+            color:Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 3
+             ),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(topLeft:Radius.circular(50))
+        ),
+        color: Color(0xffED6762),
+        onPressed: (){
+
+        },
+      ),
+    );
+
+  }
+}
+
+class _MainScroll extends StatelessWidget {
+  final items = [
+    _ListItem('Orange', Color(0xffF08F66)),
+    _ListItem('Family', Color(0xffF2A38A)),
+    _ListItem('Subscriptions', Color(0xffF7CDD5)),
+    _ListItem('Books', Color(0xffFCEBAF)),
+    _ListItem('Orange', Color(0xffF08F66)),
+    _ListItem('Family', Color(0xffF2A38A)),
+    _ListItem('Subscriptions', Color(0xffF7CDD5)),
+    _ListItem('Books', Color(0xffFCEBAF)),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        // SliverAppBar(
+        //   backgroundColor: Colors.red,
+        //   title: Text('Hola que pasa'),
+        // ),
+
+        SliverPersistentHeader(
+            floating: true, delegate: _SliverCustomHeaderDelegate(
+              minHeight: 170,
+              maxHeight: 200,
+              child: Container(
+                alignment: Alignment.centerLeft,
+                color: Colors.white,
+                child: _Titulo(),
+              )
+
+            )),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+               ...items,
+               SizedBox(height: 100,)
+            ]
+           )
+          
+          
+          )
+      ],
+    );
+  }
+}
+
+class _SliverCustomHeaderDelegate extends SliverPersistentHeaderDelegate{
+  
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+  _SliverCustomHeaderDelegate(
+    {
+    @required this.minHeight, 
+    @required this.maxHeight, 
+    @required this.child
+    });
+  
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    // TODO: implement build
+    return SizedBox.expand(child: child,);
+  }
+
+  @override
+  // TODO: implement maxExtent
+  double get maxExtent =>minHeight> maxHeight?minHeight:maxHeight;
+
+  @override
+  // TODO: implement minExtent
+  double get minExtent => minHeight;
+
+  @override
+  bool shouldRebuild( _SliverCustomHeaderDelegate oldDelegate) {
+    // TODO: implement shouldRebuild
+    return maxHeight!=oldDelegate.maxHeight||
+           minHeight!=oldDelegate.minHeight||
+           child    !=oldDelegate.child;
+  }
+
+}
+
+class _Titulo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 30,
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          child: Text(
+            'New',
+            style: TextStyle(color: Color(0xff532128), fontSize: 50),
+          ),
+        ),
+        Stack(
+          children: <Widget>[
+            SizedBox(
+              height: 60,
+            ),
+            Positioned(
+              bottom: 8,
+              child: Container(
+                width: 150,
+                height: 8,
+                decoration: BoxDecoration(color: Color(0xffF7CDD5)),
+              ),
+            ),
+            Container(
+              child: Text('List',
+                  style: TextStyle(
+                      color: Color(0xffD93A30),
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold)),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _ListadeTareas extends StatelessWidget {
+  final items = [
+    _ListItem('Orange', Color(0xffF08F66)),
+    _ListItem('Family', Color(0xffF2A38A)),
+    _ListItem('Subscriptions', Color(0xffF7CDD5)),
+    _ListItem('Books', Color(0xffFCEBAF)),
+    _ListItem('Orange', Color(0xffF08F66)),
+    _ListItem('Family', Color(0xffF2A38A)),
+    _ListItem('Subscriptions', Color(0xffF7CDD5)),
+    _ListItem('Books', Color(0xffFCEBAF)),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) => items[index]);
+  }
+}
+
+class _ListItem extends StatelessWidget {
+  final String titulo;
+  final Color color;
+
+  const _ListItem(this.titulo, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.all(30),
+      child: Text(titulo,
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20)),
+      height: 100,
+      margin: EdgeInsets.all(10),
+      decoration:
+          BoxDecoration(color: color, borderRadius: BorderRadius.circular(30)),
+    );
+  }
+}
